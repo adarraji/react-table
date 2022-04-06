c9a3d1afb99b212ce89ee5afff48954e8d1ce932
adarraji <a.darraji@outlook.com>
2022-04-05 22:11:51 -0400

    Added imports and App function

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { users } from "./users";
+import Table from "./Table";
+import { render } from "react-dom";
+
+
+const App = () => {
+    return (
+        <div>
+            <Table />
+        </div>
+    )
+}
+export default App
