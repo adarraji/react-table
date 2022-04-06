bba30c85ff4db9d900c2eea4ae2e9517bdcf523b
adarraji <a.darraji@outlook.com>
2022-04-05 22:32:09 -0400

    Added The code for Table function

--- src/Table.js
+++ src/Table.js
@@ -1,9 +1,31 @@
 import React from "react";
 
 const Table = ({ users }) => {
+    const tableData = users.map(user => {
+        return (
+            <tr>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+            </tr>
+        )
+    })
     return (
         <div>
-
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData}
+                </tbody>
+            </table>
         </div>
     )
 
