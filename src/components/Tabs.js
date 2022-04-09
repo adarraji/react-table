7d437dc497de118033c11a651f6a12d4ad9c5768
adarraji <a.darraji@outlook.com>
2022-04-09 12:33:15 -0400

    Added tab switch functions

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,13 +2,13 @@ import React from "react";
 import FirstTab from "./FirstTab";
 import SecondTab from "./SecondTab";
 
-const Tabs = ({ activeTab }) => {
+const Tabs = ({ activeTab, handleTab1, handleTab2 }) => {
     return (
         <div className="Tabs">
             {/* Tab Nav*/}
             <ul className="tab-nav">
-                <li className={activeTab === "tab1" ? "active" : ""}>Tab 1</li>
-                <li className={activeTab === "tab2" ? "active" : ""}>Tab 2</li>
+                <li className={activeTab === "tab1" ? "active" : ""} onClick={handleTab1}>Tab 1</li>
+                <li className={activeTab === "tab2" ? "active" : ""} onClick={handleTab2}>Tab 2</li>
             </ul>
             <div className="tab-content">
                 {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
