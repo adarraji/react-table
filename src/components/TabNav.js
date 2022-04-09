d46d208c826a9685e9ce03817cffd5bcae669b8a
adarraji <a.darraji@outlook.com>
2022-04-09 17:17:18 -0400

    Added TabContent components

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 const TabNav = ({ id, title, activeTab, setActiveTab }) => {
+    const onTabClick = () => setActiveTab(id)
+
     return (
-        <li onClick={setActiveTab(id)} className={activeTab === id ? "active" : ""}>
+        <li onClick={onTabClick} className={activeTab === id ? "active" : ""}>
             {title}
         </li>
     )
--- src/containers/App.js
+++ src/containers/App.js
@@ -25,13 +25,24 @@ class App extends Component {
         const { activeTab } = this.state;
         return (
             <div className="Tabs">
-                <ul tab-nav>
+                <ul className="tab-nav">
                     <TabNav title="Tab1" id="tab1" activeTab={activeTab} setActiveTab={this.setActiveTab} />
                     <TabNav title="Tab2" id="tab2" activeTab={activeTab} setActiveTab={this.setActiveTab} />
-                    <TabNav title="Tab2" id="tab2" activeTab={activeTab} setActiveTab={this.setActiveTab} />
+                    <TabNav title="Tab3" id="tab3" activeTab={activeTab} setActiveTab={this.setActiveTab} />
                 </ul>
 
-            </div>
+                <div className="tab-content">
+                    <TabContent id={"tab1"} activeTab={activeTab}>
+                        <p>Tab1 works !</p>
+                    </TabContent>
+                    <TabContent id={"tab2"} activeTab={activeTab}>
+                        <p>Tab2 works !</p>
+                    </TabContent>
+                    <TabContent id={"tab3"} activeTab={activeTab}>
+                        <p>Tab3 works !</p>
+                    </TabContent>
+                </div>
+            </div >
         )
     }
 }
