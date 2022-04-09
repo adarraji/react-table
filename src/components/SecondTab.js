e1b224caf54ce6ddb7662513f5794bc7610a66d9
adarraji <a.darraji@outlook.com>
2022-04-08 23:27:04 -0400

    Updated <p> content

--- src/components/SecondTab.js
+++ src/components/SecondTab.js
@@ -3,7 +3,7 @@ import React from "react";
 const SecondTab = () => {
     return (
         <div className="seond-tab">
-            <p>First Tab !!!</p>
+            <p>Second Tab !!!</p>
             {/* second tab content here */}
         </div>
     )
