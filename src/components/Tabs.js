671216367bb9ab316d7f5a520d250f9d2a0309db
adarraji <a.darraji@outlook.com>
2022-04-09 17:19:58 -0400

    Moved tabs code to Tab.js, Added Tabs to App

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,20 +1,46 @@
-import React from "react";
-import FirstTab from "./FirstTab";
-import SecondTab from "./SecondTab";
+import React, { Component } from "react";
+import TabNav from "./TabNav";
+import TabContent from "../components/TabContent";
 
-const Tabs = ({ activeTab, handleTab1, handleTab2 }) => {
-    return (
-        <div className="Tabs">
-            {/* Tab Nav*/}
-            <ul className="tab-nav">
-                <li className={activeTab === "tab1" ? "active" : ""} onClick={handleTab1}>Tab 1</li>
-                <li className={activeTab === "tab2" ? "active" : ""} onClick={handleTab2}>Tab 2</li>
-            </ul>
-            <div className="tab-content">
-                {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
-            </div>
-        </div>
-    );
+
+class Tabs extends Component {
+    constructor() {
+        super();
+        this.state = {
+            activeTab: "tab1"
+        }
+    }
+
+    setActiveTab = (id) => {
+        this.setState({ activeTab: id });
+    }
+
+
+
+    render() {
+        const { activeTab } = this.state;
+        return (
+            <div className="Tabs">
+                <ul className="tab-nav">
+                    <TabNav title="Tab1" id="tab1" activeTab={activeTab} setActiveTab={this.setActiveTab} />
+                    <TabNav title="Tab2" id="tab2" activeTab={activeTab} setActiveTab={this.setActiveTab} />
+                    <TabNav title="Tab3" id="tab3" activeTab={activeTab} setActiveTab={this.setActiveTab} />
+                </ul>
+
+                <div className="tab-content">
+                    <TabContent id={"tab1"} activeTab={activeTab}>
+                        <p>Tab1 works !</p>
+                    </TabContent>
+                    <TabContent id={"tab2"} activeTab={activeTab}>
+                        <p>Tab2 works !</p>
+                    </TabContent>
+                    <TabContent id={"tab3"} activeTab={activeTab}>
+                        <p>Tab3 works !</p>
+                    </TabContent>
+                </div>
+            </div >
+        )
+    }
 }
 
-export default Tabs;
+export default Tabs
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { users } from "../users";
 import Table from "../components/Table";
 import Tabs from "../components/Tabs";
-import TabNav from "../components/TabNav";
-import TabContent from "../components/TabContent";
 import "./App.css";
 
 
@@ -15,33 +13,11 @@ class App extends Component {
         }
     }
 
-    setActiveTab = (id) => {
-        this.setState({ activeTab: id });
-    }
-
-
 
     render() {
-        const { activeTab } = this.state;
         return (
-            <div className="Tabs">
-                <ul className="tab-nav">
-                    <TabNav title="Tab1" id="tab1" activeTab={activeTab} setActiveTab={this.setActiveTab} />
-                    <TabNav title="Tab2" id="tab2" activeTab={activeTab} setActiveTab={this.setActiveTab} />
-                    <TabNav title="Tab3" id="tab3" activeTab={activeTab} setActiveTab={this.setActiveTab} />
-                </ul>
-
-                <div className="tab-content">
-                    <TabContent id={"tab1"} activeTab={activeTab}>
-                        <p>Tab1 works !</p>
-                    </TabContent>
-                    <TabContent id={"tab2"} activeTab={activeTab}>
-                        <p>Tab2 works !</p>
-                    </TabContent>
-                    <TabContent id={"tab3"} activeTab={activeTab}>
-                        <p>Tab3 works !</p>
-                    </TabContent>
-                </div>
+            <div className="App">
+                <Tabs />
             </div >
         )
     }
