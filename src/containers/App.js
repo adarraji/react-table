be790dd047c19a8055eac8f555051147771a4f50
adarraji <a.darraji@outlook.com>
2022-04-08 23:16:07 -0400

    Added Tabs component tto App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { users } from "../users";
 import Table from "../components/Table";
+import Tabs from "../components/Tabs";
 
 
 const App = () => {
     return (
         <div>
+            <Tabs />
             <Table users={users} />
         </div>
     )
