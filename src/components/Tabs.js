6b3c7fdd4a0ffc32178bdc23f755a3b1013649f8
adarraji <a.darraji@outlook.com>
2022-04-08 23:19:41 -0400

    Added tab nav and tab content sections

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const Tabs = () => {
     return (
-        <div className="Tabs">
-            <p>Hello Tab Works</p>
+        <div className="tabs">
+            {/* Tab Nav*/}
+            <ul className="tab-nav">
+                <li>Tab 1</li>
+                <li>Tab 2</li>
+            </ul>
+            <div className="tab-content">
+                {/* content will be here*/}
+            </div>
         </div>
     );
 }
