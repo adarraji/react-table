8eba07f0236312684b5ab2ab994be30508ca5d60
adarraji <a.darraji@outlook.com>
2022-04-08 23:14:39 -0400

    Added Tabs component

---
+++ src/components/Tabs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tabs = () => {
+    return (
+        <div className="Tabs">
+            <p>Hello Tab Works</p>
+        </div>
+    );
+}
+
+export default Tabs;
