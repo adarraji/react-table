c99a3b3e911c5326ee5714bc3091af09bdaf9067
adarraji <a.darraji@outlook.com>
2022-04-08 23:25:06 -0400

    Added FirstTab component

---
+++ src/components/FirstTab.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstTab = () => {
+    return (
+        <div className="first-tab">
+            <p>First Tab !!!</p>
+            {/* First tab content here */}
+        </div>
+    )
+}
+
+export default FirstTab;
